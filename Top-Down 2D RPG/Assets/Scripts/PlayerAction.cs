99df840dee7e9fae206427f8f27ed5afc73e351a
yechoi0 <y_e_99@ewhain.net>
2022-02-22 21:59:42 +0900

    Animation

--- Top-Down 2D RPG/Assets/Scripts/PlayerAction.cs
+++ Top-Down 2D RPG/Assets/Scripts/PlayerAction.cs
@@ -4,24 +4,60 @@ using UnityEngine;
 
 public class PlayerAction : MonoBehaviour
 {
+    public float Speed;
+
     float h;
     float v;
+    bool isHorizonMove;
 
     Rigidbody2D rigid;
+    Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
+        // Move Value
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
+
+        // Check Button Down & Up
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+
+        // Check Horizontal Move
+        if (hDown)
+            isHorizonMove = true;
+        else if (vDown)
+            isHorizonMove = false;
+        else if (hUp || vUp)
+            isHorizonMove = h != 0;
+
+        // Animation
+        if(anim.GetInteger("hAxisRaw") != h)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("hAxisRaw", (int)h);
+        }
+        else if(anim.GetInteger("vAxisRaw") != v)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("vAxisRaw", (int)v);
+        }
+        else
+            anim.SetBool("isChange", false);
     }
 
     void FixedUpdate()
     {
-        rigid.velocity = new Vector2(h, v);
+        // Move
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = moveVec * Speed;
     }
 }
